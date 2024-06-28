dcfe5f8446ee7e4d6fdaadb84b8800f6b1994f5b
devdeepak <deepakptdr2209@gmail.com>
2024-06-28 13:06:48 +0530

    created component for counter

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,21 @@ import { useState } from 'react'
 function App() {
   const [count, setCount] = useState(0)
 
-  
-
-
-  function onButtopnclick(){
-    setCount(count + 1);
-
+  return(
+    <div>
+      <CustomButton count= {count} setCount = {setCount}></CustomButton>
+    </div>
+  )
+}
+// component for button to move counter
+// when you create component keep in mind that you write its first letter in uppercare 
+function CustomButton(props){
+  function onclickHandler(){
+    props.setCount(props.count + 1);
   }
-  return (<button onClick={onButtopnclick}>counter {count}</button>)
+  return <button onClick={onclickHandler}>
+    counter {props.count}
+    </button>
 }
 
 export default App
